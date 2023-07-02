a1df09975df98062edce52e7fbd52b91bdc164d4
DevMiks1 <MikeAlfredVargas1@gmail.com>
2023-07-02 18:32:45 +0800

    ,

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express();
+
 const port = 8000;
+const app = express();
 // middleware
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+
 
 // db connection
 const dbConfig = require("./config/db");
